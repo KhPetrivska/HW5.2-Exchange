525feca63eac749f7560445ec61e81b9bcb35903
Kristina <kristin.petrivska@gmail.com>
2024-07-17 01:40:31 -0600

    add const step

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const rate = 26;
 const dolAmountFirst = 10;
 const dolAmountLast = 100;
+const step = 10;
 let hrnAmount = '';
-for (let i = dolAmountFirst; i <= dolAmountLast; i += 10 ){
+for (let i = dolAmountFirst; i <= dolAmountLast; i += step ){
      hrnAmount = i * rate;
      console.log(`With $${i} you would get ₴${hrnAmount}`);
 }
